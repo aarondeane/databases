9ae39f5f7d6294af59af5438d1627701bc933da5
meandeane <aaronmdeane@gmail.com>
2018-12-05 17:32:36 -0800

    Updated initial server and App setup

--- server/schema.sql
+++ server/schema.sql
@@ -3,8 +3,40 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  /* Describe your table here.
+  messages will have  {user, message , room}
+  user will be linked to another table user
+  room will link to another table room */
+  id INT,
+  user VARCHAR(50),
+  text TEXT,
+  room VARCHAR(20),
+  PRIMARY KEY(id)
+  -- FOREIGN KEY(room)
+  -- REFERENCES Rooms(id),
+  -- FOREIGN KEY(user)
+  -- REFERENCES User(id)
+
 );
+-- CREATE TABLE Rooms (
+--   /* Describe your table here.
+--   messages will have  {user, message , room}
+--   user will be linked to another table user
+--   room will link to another table room */
+--   id INT,
+--   name VARCHAR(50),
+--   PRIMARY KEY(id)
+  
+-- );
+-- CREATE TABLE User (
+--   /* Describe your table here.
+--   messages will have  {user, message , room}
+--   user will be linked to another table user
+--   room will link to another table room */
+--   id INT,
+--   name VARCHAR(50),
+--   PRIMARY KEY(id)
+-- );
 
 /* Create other tables and define schemas for them here! */
 
--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -16,7 +16,7 @@ describe('Persistent Node Chat Server', function() {
     });
     dbConnection.connect();
 
-       var tablename = ""; // TODO: fill this out
+       var tablename = "messages"; // TODO: fill this out
 
     /* Empty the db table before each test so that multiple tests
      * (or repeated runs of the tests) won't screw each other up: */
@@ -29,12 +29,14 @@ describe('Persistent Node Chat Server', function() {
 
   it('Should insert posted messages to the DB', function(done) {
     // Post the user to the chat server.
+    console.log('a')
     request({
       method: 'POST',
       uri: 'http://127.0.0.1:3000/classes/users',
       json: { username: 'Valjean' }
     }, function () {
       // Post a message to the node chat server:
+      console.log('b')
       request({
         method: 'POST',
         uri: 'http://127.0.0.1:3000/classes/messages',
